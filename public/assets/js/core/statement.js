03e8c6ed72924a5572a0ba208d0f2152818dcd7c
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2020-05-05 03:27:12 +0530

    Problem Statement API Client Side + Server Side Refer #4

--- public/assets/js/core/statement.js
+++ public/assets/js/core/statement.js
@@ -1,1 +1,41 @@
 // problem statement related js
+
+let getStatement = async function(level) {
+	let token = localStorage.getItem("qid-token");
+	let statement;
+	if(token != firebase.auth().currentUser.uid)
+	{
+		localStorage.removeItem(level);
+		localStorage.setItem("qid-token",firebase.auth().currentUser.uid);
+	}
+	if(token)
+	{
+		statement = localStorage.getItem(level);
+	}
+	if(!statement)
+	{
+		serverResp = await fetch('http://localhost:5001/csbs-snu/us-central1/statement?level='+level);
+		statement = await serverResp.json();
+		statement.submission = {
+			input: "storage_bucket_url/" + firebase.auth().currentUser.uid,
+		};
+		localStorage.setItem(level,statement);
+	}
+	return statement;
+}
+
+firebase.auth().onAuthStateChanged(function(user)
+{
+	if(user)
+	{
+		getStatement("tetris").then((statement) => {
+			// you got the result. 
+			console.log(statement);
+		}).catch( e => {
+			// handle error here
+			// e has the error code
+		});
+	}
+});
+
+
